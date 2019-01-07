ef0804a16524c96b7d1378b28de85fb17289f5ff
Pablo <pablojr1971@googlemail.com>
2019-01-07 07:48:51 +0000

    Second

--- absoluteValuesSumMinimization.py
+++ absoluteValuesSumMinimization.py
@@ -1,5 +1,11 @@
 def absoluteValuesSumMinimization(a):
-    return min([abs(p - x) for p in a for x in a])
-
-
+    #return min([abs(p - x) for p in a for x in a])
+    ls=[]
+    for x in a:
+        count=0
+        for p in a:
+            count+=abs(p-x)
+        ls.append((x,count))
+    d=dict(ls)
+    return min(d, key=d.get)
 print(absoluteValuesSumMinimization([ 2, 4, 7]))
